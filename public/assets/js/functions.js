8d96c1c03250a8218d913f138ec81a2eb7d33ce0
Perla Showell <perlashowell@gmail.com>
2017-12-19 01:09:15 -0500

    stars, routing view profile, and alignment of profile widget

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -259,10 +259,26 @@ $(document).ready(function () {
     });
 
 // star rating (espie)
-$('.rating input').change(function () {
-  var $radio = $(this);
-  $('.rating .selected').removeClass('selected');
-  $radio.closest('label').addClass('selected');
+var $star_rating = $('.star-rating .fa');
+
+var SetRatingStar = function() {
+  return $star_rating.each(function() {
+    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+      return $(this).removeClass('fa-star-o').addClass('fa-star');
+    } else {
+      return $(this).removeClass('fa-star').addClass('fa-star-o');
+    }
+  });
+};
+
+$star_rating.on('click', function() {
+  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+  return SetRatingStar();
+});
+
+SetRatingStar();
+$(document).ready(function() {
+
 });
 
 });
